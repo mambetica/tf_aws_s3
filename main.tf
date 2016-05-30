2e59fdad2ef7f4faea496cc1b5d17bb182bab4ed
Andy A <mambetica@hotmail.com>
2016-05-30 22:00:28 +0100

    Add policy

--- main.tf
+++ main.tf
@@ -1,6 +1,22 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "${var.bucket}"
   acl = "${var.acl}"
+  policy = <<EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "ELB PutObject to S3 Bucket",
+			"Effect": "Allow",
+			"Principal": {
+				"AWS": "*"
+			},
+			"Action": "s3:PutObject",
+			"Resource": "arn:aws:s3:::${var.bucket}/*"
+		}
+	]	
+}
+EOF
   tags {
     Name = "${var.bucket}"
     Owner = "${var.owner}"
