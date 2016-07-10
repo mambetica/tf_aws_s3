947ed9d244178b1a9bff6e053468c9aa51d8e5b7
Andy A <mambetica@hotmail.com>
2016-07-10 19:06:55 +0100

    Added lifecycle rules

--- variables.tf
+++ variables.tf
@@ -21,3 +21,7 @@ variable "policy_file" {
   description = "Policy file to provide the bucket policy"
   default = ""
 }
+variable "lifecycle_rule_file" {
+  description = "Lifecycle rule file to provide the lifecycle rules for the bucket"
+  default = "{}"
+}
